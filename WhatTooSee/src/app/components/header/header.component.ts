df096f7b032ad9ae8c93d16b0cf60aacf8f72663
Edson Ibarra <edsonibarra54@gmail.com>
2024-03-28 01:48:34 -0600

    Fixed the responsiveness of the navbar and added the burger menu functionality

--- WhatTooSee/src/app/components/header/header.component.ts
+++ WhatTooSee/src/app/components/header/header.component.ts
@@ -1,13 +1,43 @@
-import { Component } from '@angular/core';
+import { NgIf } from '@angular/common';
+import { Component, ElementRef, Renderer2, AfterViewInit } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [ RouterOutlet, RouterLink ],
+  imports: [ RouterOutlet, RouterLink, NgIf ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
-export class HeaderComponent {
+export class HeaderComponent implements AfterViewInit{
+  public isSecondListVisible: boolean = false;
 
+  constructor(private elRef: ElementRef, private renderer: Renderer2) {}
+
+  ngAfterViewInit(): void {
+    const menuTop = this.elRef.nativeElement.querySelector('#menu-top');
+    const secondList = this.elRef.nativeElement.querySelector('#secondListDiv');
+    let h: number;
+
+    const observerMenuTop = new ResizeObserver(entries => {
+      for (const entry of entries) {
+        const width = entry.contentRect.width;
+        h = width / 13.65;
+        console.log('Calculated height:', h);
+        this.renderer.setStyle(menuTop, 'height', `${h}px`);
+        this.renderer.setStyle(secondList, 'margin-top', `${h}px`);
+      }
+    });
+
+    observerMenuTop.observe(menuTop);
+  }
+
+  public toggleSecondList(): void {
+    this.isSecondListVisible = !this.isSecondListVisible;
+
+    const secondList = this.elRef.nativeElement.querySelector('#secondListDiv');
+
+    const displayValue = this.isSecondListVisible ? 'flex' : 'none';
+    this.renderer.setStyle(secondList, 'display', displayValue);
+  }
 }
