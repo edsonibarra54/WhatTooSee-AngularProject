8f59b6783591350e1806ddf3892f31ba5adf1d8b
Edson Ibarra <edsonibarra54@gmail.com>
2024-05-09 15:13:26 -0600

    Deleted the web server folder

--- webserver/app.js
+++
@@ -1,9 +0,0 @@
-const http = require("http");
-
-http.createServer( (req, res) => {
-    if(req.method == "GET")
-        res.write("Hola a todos");
-    else if(req.method == "POST")
-        res.write("Hola mundo");
-    res.end();
-} ).listen(8080);
